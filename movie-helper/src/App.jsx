40272c9df4871239b74f7f52dfac1be7b840a7cb
r4luthra <r4luthra@uwaterloo.ca>
2025-06-21 11:29:51 -0400

    Initial commit - movies being fetched

---
+++ movie-helper/src/App.jsx
@@ -0,0 +1,83 @@
+import { useEffect, useState } from 'react'
+import './App.css'
+import Search from './components/search'
+import axios from 'axios'
+import Spinner from './components/spinner';
+
+const API_URL = import.meta.env.VITE_API_BASE_URL;
+const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
+const API_OPTIONS = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: `Bearer ${API_KEY}`
+  }
+
+}
+const App = () => {
+  const [searchTerm, setSearchTerm] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+  const [movieList, setMovieList] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const fetchMovies = async () => {
+    setIsLoading(true);
+    setErrorMessage('');
+    try {
+      const endpoint = `${API_URL}/discover/movie?sort_by=popularity.desc`
+      const response = await axios.get(endpoint, API_OPTIONS)
+      if (response.data === "False") {
+        setErrorMessage(response.data.Error || "something went wrong while fetching movies");
+        setMovieList([]);
+      } else {
+        setMovieList(response.data.results);
+      } 
+      return response.data;
+    } catch (error) {
+      console.log("Error fetching movies: ", error);
+      setErrorMessage("Error fetching movies. Please try again later.");
+      return null;
+    } finally {
+      setIsLoading(false);
+    }
+  }
+  useEffect(() => {
+    fetchMovies();
+  }, []);
+  return (
+    <main>
+      <div className="pattern" />
+
+      <div className="wrapper">
+        <header>
+          <img src="./hero-img.png" alt="Hero Banner" />
+          <h1>
+            Find <span className="text-gradient"> Movies </span> You'll Enjoy
+          </h1>
+
+          <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+        </header>
+
+        <section className="all-movies">
+          <h2 className="mt-[40px]">
+            All Movies
+          </h2>
+          
+          { isLoading ? (
+            <Spinner />
+          ) : errorMessage ? (
+            <p className="text-red-500"> { errorMessage } </p>
+          ) : (
+            <ul>
+              { movieList.map((movie) => (
+                <p key={movie.id} className="text-white">{ movie.title }</p>
+              ))}
+            </ul>
+          )
+          }
+        </section>
+      </div>
+    </main>
+  )
+}
+
+export default App
